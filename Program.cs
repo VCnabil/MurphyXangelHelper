1a0d81c7d67c42765297d75ea90f9d45c7a676c8
Nabil <nlamriben@vcinc.com>
2024-10-04 11:52:38 -0400

    one page app

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace MurphyXangelHelper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MurphyXangelForm());
         }
     }
 }
